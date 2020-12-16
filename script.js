8a4b9bd6296f662100d181316e2737351ebabc2e
Claire Cheek <claireccheek@gmail.com>
2020-12-15 23:21:26 -0800

    i tried

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("meow")
+
+
+//create opening screen welcoming user to quiz
+    //create button to start quiz
+        //TODO: create function that presents question and answers when "start quiz" button is pushed
+
+    // function startQuiz()
+
+// create five questions with four possible answers and correct answer
+var qAndA = [
+{
+
+    question: `Who said “I’m trying very hard not to connect with people right now.”`, 
+    answers: ["Alexis", "David", "Moira", "Stevie"],
+    correct: "David"
+ },
+ {
+
+    question: `Who said “Oh, I’d kill for a good coma right now.”`,
+    answers: ["David", "Moira", "Stevie", "Johnny"],
+    correct: "Moira"
+ },
+ {
+ 
+    question:`Who said "But people love extreme vanity! And they love puppies!"`,
+    answers:["Alexis", "David" ,"Moira", "Johnny"],
+    correct:"Alexis"
+ },
+ {
+
+    question:`Who said "Hashtag, is that two words?"`,
+    answers:["Moira" ,"Stevie" ,"Johnny", "Alexis"],
+    correct: "Johnny"
+ },
+ {
+
+ 
+    question:`Who said "I'm incapable of faking sincerity."`,
+    answers:["Alexis", "David", "Moira", "Stevie"],
+    correct:"Stevie"
+ }
+];
+
+
+var quizBodyDiv = document.getElementById("")
+
+        
+
+//TODO: create a timer set to start when user starts quiz
+
+    //TODO: when user gets question wrong take 5 seconds from timer
+        //TODO:when user gets question right give 5 seconds to timer
+
+
+//TODO:create scoreboard
+        //TODO: log users score to local and post to scoreboard
