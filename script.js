f2271728f6f29ad92e63ab024addea3f45d28fe7
Claire Cheek <claireccheek@gmail.com>
2020-12-25 22:11:55 -0800

    timer

--- script.js
+++ script.js
@@ -1,52 +1,75 @@
 
 
 //create opening screen welcoming user to quiz
-    //create button to start quiz
-        //TODO: create function that presents question and answers when "start quiz" button is pushed
+//create button to start quiz
 
-    // function startQuiz()
+//TODO: create function that presents question and answers when "start quiz" button is pushed
+
+// function startQuiz()
 
 // create five questions with four possible answers and correct answer
 var qAndA = [
-{
+   {
 
-    question: `Who said “I’m trying very hard not to connect with people right now.”`, 
-    answers: ["Alexis", "David", "Moira", "Stevie"],
-    correct: "David"
- },
- {
+      question: `Who said “I’m trying very hard not to connect with people right now.”`,
+      answers: ["Alexis", "David", "Moira", "Stevie"],
+      correct: "David"
+   },
+   {
 
-    question: `Who said “Oh, I’d kill for a good coma right now.”`,
-    answers: ["David", "Moira", "Stevie", "Johnny"],
-    correct: "Moira"
- },
- {
- 
-    question:`Who said "But people love extreme vanity! And they love puppies!"`,
-    answers:["Alexis", "David" ,"Moira", "Johnny"],
-    correct:"Alexis"
- },
- {
+      question: `Who said “Oh, I’d kill for a good coma right now.”`,
+      answers: ["David", "Moira", "Stevie", "Johnny"],
+      correct: "Moira"
+   },
+   {
 
-    question:`Who said "Hashtag, is that two words?"`,
-    answers:["Moira" ,"Stevie" ,"Johnny", "Alexis"],
-    correct: "Johnny"
- },
- {
+      question: `Who said "But people love extreme vanity! And they love puppies!"`,
+      answers: ["Alexis", "David", "Moira", "Johnny"],
+      correct: "Alexis"
+   },
+   {
 
- 
-    question:`Who said "I'm incapable of faking sincerity."`,
-    answers:["Alexis", "David", "Moira", "Stevie"],
-    correct:"Stevie"
- }
+      question: `Who said "Hashtag, is that two words?"`,
+      answers: ["Moira", "Stevie", "Johnny", "Alexis"],
+      correct: "Johnny"
+   },
+   {
+
+
+      question: `Who said "I'm incapable of faking sincerity."`,
+      answers: ["Alexis", "David", "Moira", "Stevie"],
+      correct: "Stevie"
+   }
 ];
 
 
 var quizBodyDiv = document.getElementById("")
 
-        
+
 
 //TODO: create a timer set to start when user starts quiz
+
+var timeEl = document.body.children[5]
+var secondsLeft = 60;
+
+function setTimer() {
+   //create countdown timer
+   var countDown = setInterval(function (){
+      //countdown
+      secondsLeft--;
+      //display on page
+      timeEl.textContent = secondsLeft;
+      //stops timer at 0
+      if (secondsLeft === 0) {
+         clearInterval(countDown);
+         
+      }
+      //counts by 1 second
+   }, 1000);
+
+}
+//calls the setTimer function
+setTimer();
 
     //TODO: when user gets question wrong take 5 seconds from timer
         //TODO:when user gets question right give 5 seconds to timer
