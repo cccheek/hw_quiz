5fb3d0a1911b8c971dd86602fcab310e31500b40
Claire Cheek <claireccheek@gmail.com>
2020-12-27 19:04:04 -0800

    start button clickevent

--- script.js
+++ script.js
@@ -2,43 +2,53 @@
 
 //create opening screen welcoming user to quiz
 //create button to start quiz
-
 //TODO: create function that presents question and answers when "start quiz" button is pushed
+var startBtn = document.querySelector("#start")
+
+startBtn.addEventListener("click", function(){
+   console.log("start!")
+
+})
+
+
+
+
+
 
 // function startQuiz()
 
 // create five questions with four possible answers and correct answer
-var qAndA = [
+var questionsAndAnswerOb = [
    {
 
-      question: `Who said “I’m trying very hard not to connect with people right now.”`,
-      answers: ["Alexis", "David", "Moira", "Stevie"],
-      correct: "David"
+      question1: `Who said “I’m trying very hard not to connect with people right now.”`,
+      answers1: ["Alexis", "David", "Moira", "Stevie"],
+      correct1: "David"
    },
    {
 
-      question: `Who said “Oh, I’d kill for a good coma right now.”`,
-      answers: ["David", "Moira", "Stevie", "Johnny"],
-      correct: "Moira"
+      question2: `Who said “Oh, I’d kill for a good coma right now.”`,
+      answers2: ["David", "Moira", "Stevie", "Johnny"],
+      correct2: "Moira"
    },
    {
 
-      question: `Who said "But people love extreme vanity! And they love puppies!"`,
-      answers: ["Alexis", "David", "Moira", "Johnny"],
-      correct: "Alexis"
+      question3: `Who said "But people love extreme vanity! And they love puppies!"`,
+      answers3: ["Alexis", "David", "Moira", "Johnny"],
+      correct3: "Alexis"
    },
    {
 
-      question: `Who said "Hashtag, is that two words?"`,
-      answers: ["Moira", "Stevie", "Johnny", "Alexis"],
-      correct: "Johnny"
+      question4: `Who said "Hashtag, is that two words?"`,
+      answers4: ["Moira", "Stevie", "Johnny", "Alexis"],
+      correct4: "Johnny"
    },
    {
 
 
-      question: `Who said "I'm incapable of faking sincerity."`,
-      answers: ["Alexis", "David", "Moira", "Stevie"],
-      correct: "Stevie"
+      question5: `Who said "I'm incapable of faking sincerity."`,
+      answers5: ["Alexis", "David", "Moira", "Stevie"],
+      correct5: "Stevie"
    }
 ];
 
@@ -49,27 +59,27 @@ var quizBodyDiv = document.getElementById("")
 
 //TODO: create a timer set to start when user starts quiz
 
-var timeEl = document.body.children[5]
-var secondsLeft = 60;
+// var timeEl = document.body.children[5]
+// var secondsLeft = 60;
 
-function setTimer() {
-   //create countdown timer
-   var countDown = setInterval(function (){
-      //countdown
-      secondsLeft--;
-      //display on page
-      timeEl.textContent = secondsLeft;
-      //stops timer at 0
-      if (secondsLeft === 0) {
-         clearInterval(countDown);
+// function setTimer() {
+//    //create countdown timer
+//    var countDown = setInterval(function (){
+//       //countdown
+//       secondsLeft--;
+//       //display on page
+//       // timeEl.textContent = secondsLeft;
+//       //stops timer at 0
+//       if (secondsLeft === 0) {
+//          clearInterval(countDown);
          
-      }
-      //counts by 1 second
-   }, 1000);
+//       }
+//       //counts by 1 second
+//    }, 1000);
 
-}
-//calls the setTimer function
-setTimer();
+// }
+// //calls the setTimer function
+// setTimer();
 
     //TODO: when user gets question wrong take 5 seconds from timer
         //TODO:when user gets question right give 5 seconds to timer
